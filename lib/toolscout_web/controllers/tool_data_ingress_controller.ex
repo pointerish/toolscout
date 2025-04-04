df19c1444a00b4fe9c5bcacd7588181dd35a96f1
Josias Alvarado <josiasjag@gmail.com>
2025-04-04 14:52:55 -0600

    fix: refactor controller to handle connections errors to tool data source url

--- lib/toolscout_web/controllers/tool_data_ingress_controller.ex
+++ lib/toolscout_web/controllers/tool_data_ingress_controller.ex
@@ -7,11 +7,17 @@ defmodule ToolscoutWeb.ToolDataIngress do
   def run(conn, params) do
     data = Map.get(params, "data")
     leachy_link = ToolDataIngress.extract_leachy_link(data)
-    dbg(leachy_link)
-    raw_tool_data = ToolDataIngress.extract_leachy_raw_data(leachy_link, :curl)
-    dbg(raw_tool_data)
+    raw_tool_data = ToolDataIngress.extract_leachy_raw_data(leachy_link)
 
+    case raw_tool_data do
+      {:error, reason} ->
+        json(conn, %{error: "Failed to extract raw data: #{reason}"})
+      _ ->
+        process_tool_data(conn, raw_tool_data)
+    end
+  end
 
+  defp process_tool_data(conn, raw_tool_data) do
     case ToolDataIngress.is_tool_batch_new?(raw_tool_data) do
       true ->
         {new_added_tools_count, _} = Gpt.process_prompt(raw_tool_data)
